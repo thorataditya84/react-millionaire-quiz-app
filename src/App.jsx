9602a1b71a1d8d325571949680267c617e1cd568
Aditya Thorat <thorat.aditya@outlook.com>
2022-06-09 15:57:14 +0530

    complete react-quiz-app

--- src/App.jsx
+++ src/App.jsx
@@ -1,104 +1,130 @@
-import { useEffect, useState } from "react";
 import "./app.css";
-import Question from "./components/Questions"
-import Timer from "./components/Timer"
+import { useEffect, useMemo, useState } from "react";
+import Start from "./components/Start";
+import Timer from "./components/Timer";
+import Questions from "./components/Questions";
 
 
 function App() {
-
+    const [username, setUsername] = useState(null);
+    const [timeOut, setTimeOut] = useState(false);
     const [questionNumber, setQuestionNumber] = useState(1);
-
-    const [stop, setStop] = useState(false);
-
     const [earned, setEarned] = useState("$ 0");
 
-    const questionsData = [{
-        id: 1,
-        question: "Rolex is a company that specializes in what type of product?",
-        answers: [
-            {text: "Phone", correct: false},
-            {text: "Watches", correct: true},
-            {text: "Food", correct: false},
-            {text: "Cosmetic", correct: false},
-        ]}, {
-        id: 2,
-        question: "When did the website `Facebook` launch?",
-        answers: [
-            {text: "2004", correct: true},
-            {text: "2005", correct: false},
-            {text: "2006", correct: false},
-            {text: "2007", correct: false},
-        ]}, {
-        id: 3,
-        question: "Who played the character of harry potter in movie?",
-        answers: [
-            {text: "Johnny Deep", correct: false},
-            {text: "Leonardo Di Caprio", correct: false},
-            {text: "Denzel Washington", correct: false},
-            {text: "Daniel Red Cliff", correct: true}
-        ]},
+    const data = [
+        {
+            id: 1,
+            question: "Rolex is a company that specializes in what type of product?",
+            answers: [
+                {text: "Phone", correct: false},
+                {text: "Watches", correct: true},
+                {text: "Food", correct: false},
+                {text: "Cosmetic", correct: false}
+            ],
+        },
+        {
+            id: 2,
+            question: "When did the website `Facebook` launch?",
+            answers: [
+                {text: "2004", correct: true},
+                {text: "2005", correct: false},
+                {text: "2006", correct: false},
+                {text: "2007", correct: false,
+            },
+            ],
+        },
+        {
+            id: 3,
+            question: "Who played the character of harry potter in movie?",
+            answers: [
+                {text: "Johnny Deep", correct: false},
+                {text: "Leonardo Di Caprio", correct: false},
+                {text: "Denzel Washington", correct: false},
+                {text: "Daniel Red Cliff", correct: true},
+            ],
+        },
     ];
 
-    const money = [
-        {number: 1, amount: "$ 100"},
-        {number: 2, amount: "$ 200"},
-        {number: 3, amount: "$ 300"},
-        {number: 4, amount: "$ 500"},
-        {number: 5, amount: "$ 1000"},
-        {number: 6, amount: "$ 2000"},
-        {number: 7, amount: "$ 4000"},
-        {number: 8, amount: "$ 8000"},
-        {number: 9, amount: "$ 16000"},
-        {number: 10, amount: "$ 32000"},
-        {number: 11, amount: "$ 64000"},
-        {number: 12, amount: "$ 125000"},
-        {number: 13, amount: "$ 250000"},
-        {number: 14, amount: "$ 500000"},
-        {number: 15, amount: "$ 1000000"}
-    ].reverse();
+    const moneyPyramid = useMemo (
+        () => [
+            { id: 1, amount: "$ 100" },
+            { id: 2, amount: "$ 200" },
+            { id: 3, amount: "$ 300" },
+            { id: 4, amount: "$ 500" },
+            { id: 5, amount: "$ 1.000" },
+            { id: 6, amount: "$ 2.000" },
+            { id: 7, amount: "$ 4.000" },
+            { id: 8, amount: "$ 8.000" },
+            { id: 9, amount: "$ 16.000" },
+            { id: 10, amount: "$ 32.000" },
+            { id: 11, amount: "$ 64.000" },
+            { id: 12, amount: "$ 125.000" },
+            { id: 13, amount: "$ 250.000" },
+            { id: 14, amount: "$ 500.000" },
+            { id: 15, amount: "$ 1.000.000" },
+        ].reverse(), []
+    );
 
     useEffect(() => {
-        questionNumber > 1 && 
-        setEarned(money.find((m) => m.id === questionNumber - 1).amount);
-    }, [money, questionNumber]);
+        questionNumber > 1 &&
+        setEarned(moneyPyramid.find((m) => m.id === questionNumber - 1).amount);
+    }, [questionNumber, moneyPyramid]);
+
 
     return (
         <div className="app">
-            <div className="main">
-                {
-                    stop ? (
-                        <h1 className="endText">You earned: {earned}</h1> 
-                    ) : (
-                        <>
-                            <div className="top">
-                                <div className="timer">
-                                    <Timer setStop={setStop} questionNumber={questionNumber} />
-                                </div>
-                            </div>
-                            <div className="bottom">
-                                <Question
-                                    questionsData={questionsData}
-                                    setStop={setStop}
-                                    questionNumber={questionNumber}
-                                    setQuestionNumber={setQuestionNumber}
-                                />
-                            </div>
-                        </>
-                    )
-                }
-            </div>
-            <div className="pyramid">
-                <ul className="moneyList">
-                    {
-                        money.map(element => (
-                            <li className={questionNumber === element.number ? "moneyListItem active" : "moneyListItem"}>
-                                <span className="moneyListItemNumber">{element.number}</span>
-                                <span className="moneyListItemAmount">{element.amount}</span>
-                            </li>
-                        ))
-                    }
-                </ul>
-            </div>
+            {
+                !username ? (
+                    <Start setUsername={setUsername} />
+                ) : (
+                    <>
+                        <div className="main">
+                            {
+                                timeOut ? (
+                                    <h1 className="endText">You earned: {earned}</h1>
+                                ) : (
+                                    <>
+                                        <div className="top">
+                                            <div className="timer">
+                                                <Timer
+                                                setTimeOut={setTimeOut}
+                                                questionNumber={questionNumber}
+                                                />
+                                            </div>
+                                        </div>
+                                        <div className="bottom">
+                                        <Questions
+                                            data={data}
+                                            questionNumber={questionNumber}
+                                            setQuestionNumber={setQuestionNumber}
+                                            setTimeOut={setTimeOut}
+                                        />
+                                        </div>
+                                    </>
+                                )
+                            }
+                        </div>
+                        <div className="pyramid">
+                            <ul className="moneyList">
+                                {
+                                    moneyPyramid.map((m) => (
+                                        <li
+                                            className={
+                                                questionNumber === m.id
+                                                ? "moneyListItem active"
+                                                : "moneyListItem"
+                                            }
+                                        >
+                                            <span className="moneyListItemNumber">{m.id}</span>
+                                            <span className="moneyListItemAmount">{m.amount}</span>
+                                        </li>
+                                    ))
+                                }
+                            </ul>
+                        </div>
+                    </>
+                )
+            }
         </div>
     );
 }
